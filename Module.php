ae5bb14c8afe49935dbad846ab50e324e9ae486e
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:48 +0200

    Refactored module settings

--- Module.php
+++ Module.php
@@ -56,6 +56,15 @@ class Module extends \Aurora\System\Module\AbstractModule
         return parent::Decorator();
     }
 
+    /**
+     *
+     * @return Settings
+     */
+    protected function GetModuleSettings()
+    {
+        return $this->oModuleSettings;
+    }
+
     public function onAddToContentSecurityPolicyDefault($aArgs, &$aAddDefault)
     {
         $aAddDefault[] = 'www.google.com www.gstatic.com';
